6e4d6f32cb5adf5489fe883ef5e64c302c5fe1c9
pdoha <https://github.com/pdoha>
2024-02-14 23:47:29 +0900

    전체 접근 가능한 페이지 & 로그인시 접근 가능한 페이지 설정 & jsondata 오류 해결 완료

--- src/main/java/org/project/commons/rests/JSONData.java
+++ src/main/java/org/project/commons/rests/JSONData.java
@@ -1,14 +1,13 @@
 package org.project.commons.rests;
 
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.http.HttpStatus;
 
 
+
 @Data
-@NoArgsConstructor @RequiredArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
 //생성자 오류 생김
 public class JSONData {
     //성공했을때
@@ -16,7 +15,7 @@ public class JSONData {
     //성공시 응답코드
     private HttpStatus status = HttpStatus.OK;
     //성공시 데이터
-    @NotNull
+    @NonNull
     private Object data;
     //실패시 문제 메세지 ( 한개일수도, 여러개일 수도 있으니까 object)
     private Object message;
